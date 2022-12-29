f3f182fc8500fff9949c19be4972de409dbc79c6
Xodein <eraykrdg1@gmail.com>
2022-12-29 23:08:21 +0300

    asdasdwazawaz

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,22 +3,29 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WebAssignment.Context;
 using WebAssignment.Models;
+using WebAssignment.ViewModels;
 
 namespace WebAssignment.Controllers
 {
     public class HomeController : Controller
     {   
         private readonly moviesContext movcontext;
-        public HomeController(moviesContext movcontext)
+        private readonly newsContext newscontext;
+        public HomeController(moviesContext movcontext, newsContext newscontext)
         {
             this.movcontext = movcontext;
+            this.newscontext = newscontext;
         }
-
         public async Task<IActionResult> Index()
         {
             var movs = await movcontext.movies.ToListAsync();
             var content = movs.Where(i => i.ispopular == true);
-            return View(content);
+            var _news = await newscontext.news.ToListAsync();
+
+            var vwmodel = new viewmodel();
+            vwmodel.mvs = content;
+            vwmodel.nws = _news;
+            return View(vwmodel);
         }
         public async Task<IActionResult> DetailsAsync(int? id)
         {
---
+++ ViewModels/viewmodel.cs
@@ -0,0 +1,10 @@
+﻿using WebAssignment.Models;
+
+namespace WebAssignment.ViewModels
+{
+    public class viewmodel
+    {
+        public IEnumerable<movies> mvs { get; set; }
+        public IEnumarable<news> nws { get; set; }
+    }
+}
